c5d422e2df2cbea6415f25e6a9c04d94db55037a
Sam Patterson <pattersonrsam@gmail.com>
2024-10-21 23:15:37 -0400

    time update

--- src/app/loser/page.tsx
+++ src/app/loser/page.tsx
@@ -116,7 +116,7 @@ export default function Loser() {
     if (
       now.getDay() === 1 &&
       now.getHours() === 23 &&
-      now.getMinutes() === 29
+      now.getMinutes() === 44
     ) {
       const randomService = new RandomService();
       randomService.setRandomNumber(
@@ -135,7 +135,7 @@ export default function Loser() {
     if (
       now.getDay() === 1 &&
       now.getHours() === 23 &&
-      now.getMinutes() === 30
+      now.getMinutes() === 45
     ) {
       spinWheelRef.current?.();
     }
